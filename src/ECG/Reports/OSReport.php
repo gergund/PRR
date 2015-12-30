225d5cdada222fa18693f3dcb14b5795f4cb667b
Denys Gergun <gergund@gmail.com>
2015-12-30 14:41:48 +0200

    Report class example test 2

--- src/ECG/Reports/OSReport.php
+++ src/ECG/Reports/OSReport.php
@@ -11,6 +11,7 @@ namespace ECG\Reports;
 use ECG\Infos\OSInfo;
 
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -21,22 +22,17 @@ class OSReport {
         $os_info = new OSInfo();
         $kernel = $os_info->getKernel();
         $hostname = $os_info->getHostName();
-        $info = 'Linux version: '.$kernel.' Hostname: '.$hostname;
-        $text = "Application execution statement: ".$info;
-        $output->writeln($text);
 
         $output->writeln('');$output->writeln('');
         $output->writeln('Table:');
 
-        $rows = 10;
         $table = new Table($output);
-        for ($i = 0; $i<$rows; $i++) {
-            $table->addRow([
-                sprintf('Row <info># %s</info>', $i),
-                rand(0, 1000)
-            ]);
+        $table->setHeaders(array(
+            array(new TableCell('OS parameters'),array('colspan' => 3))
+        ));
+        $table->addRow(sprintf('Kernel version: <info>%s</info>',$kernel));
+        $table->addRow(sprintf('Hostname: <info>%s</info>',$hostname));
 
-        }
         $table->render();
     }
 
