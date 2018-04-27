04db6aebdbba98674da17625ce5581ebaaac94ee
Denys Gergun <dgergun@magento.com>
2018-04-27 17:07:34 +0300

    New commits

--- src/ECG/Infos/MageInfo.php
+++ src/ECG/Infos/MageInfo.php
@@ -35,7 +35,110 @@ class MageInfo
 
         return ''.$mageversion.' '.$magedistr;
 
+    }
 
+    public function getSession($magedir)
+    {
+        $file = $magedir.'/app/etc/env.php';
+
+        if (!is_file($file) || !is_readable($file)) {
+            return 'Unknown';
+        }
+
+        $mage_conf=require($file);
+
+        if ($mage_conf['session']['save'] == 'redis'){
+            return 'Redis';
+        }elseif ($mage_conf['session']['save'] == 'files'){
+            return 'Files';
+        }else{
+            return 'Unknown';
+        }
+    }
+
+    public function getCache($magedir)
+    {
+        $file = $magedir.'/app/etc/env.php';
+
+        if (!is_file($file) || !is_readable($file)) {
+            return 'Unknown';
+        }
+
+        $mage_conf=require($file);
+
+        if (isset($mage_conf['cache']['frontend']['default']['backend'])) {
+
+            if ($mage_conf['cache']['frontend']['default']['backend'] == 'Cm_Cache_Backend_Redis') {
+                return 'Redis';
+            } else {
+                return 'Unknown';
+            }
+        }else{
+            return 'Files';
+        }
+
+    }
+
+    public function getPageCache($magedir)
+    {
+        $file = $magedir.'/app/etc/env.php';
+
+        if (!is_file($file) || !is_readable($file)) {
+            return 'Unknown';
+        }
+
+        $mage_conf=require($file);
+
+        if (isset($mage_conf['cache']['frontend']['page_cache']['backend'])) {
+
+            if ($mage_conf['cache']['frontend']['page_cache']['backend'] == 'Cm_Cache_Backend_Redis') {
+                return 'Redis';
+            } else {
+                return 'Unknown';
+            }
+        }else {
+            return 'Files';
+        }
+
+    }
+
+    protected function returnEmptyCacheType()
+    {
+        $cache_type = array(
+            'config' => 'Unknown',
+            'layout' => 'Unknown',
+            'block_html' => 'Unknown',
+            'collections' => 'Unknown',
+            'reflection' => 'Unknown',
+            'db_ddl' => 'Unknown',
+            'eav' => 'Unknown',
+            'customer_notification' => 'Unknown',
+            'config_integration' => 'Unknown',
+            'config_integration_api' => 'Unknown',
+            'target_rule' => 'Unknown',
+            'full_page' => 'Unknown',
+            'translate' => 'Unknown',
+            'config_webservice' => 'Unknown',
+            'compiled_config' => 'Unknown',
+        );
+        return $cache_type;
+    }
+
+    public function getCacheType($magedir)
+    {
+        $file = $magedir.'/app/etc/env.php';
+
+        if (!is_file($file) || !is_readable($file)) {
+            return $this->returnEmptyCacheType();
+        }
+
+        $mage_conf=require($file);
+        if(isset($mage_conf['cache_types']['config']))
+        {
+                    return $mage_conf['cache_types'];
+        }else{
+            return $this->returnEmptyCacheType();
+        }
 
     }
 }
--- src/ECG/Infos/PHPInfo.php
+++ src/ECG/Infos/PHPInfo.php
@@ -15,4 +15,46 @@ class PHPInfo
     {
         return phpversion();
     }
+
+    public  function  getDisplayErrors()
+    {
+       return ini_get('display_errors');
+    }
+
+    public function getMemoryLimit()
+    {
+        return ini_get('memory_limit');
+    }
+
+    public function getLogErrors()
+    {
+        return ini_get('log_errors');
+    }
+
+    public function getMaxExecution()
+    {
+        return ini_get('max_execution_time');
+    }
+
+    public function getMaxInput()
+    {
+        return ini_get('max_input_time');
+    }
+
+    public function getOpcacheMemoryConsumption()
+    {
+        $max_consumption = opcache_get_configuration()['directives']['opcache.memory_consumption'];
+        return sprintf('%s MB ',$max_consumption/1024/1024);
+    }
+
+    public function getOpcacheMaxAcceleratedFiles()
+    {
+        $max_files = opcache_get_configuration()['directives']['opcache.max_accelerated_files'];
+        return sprintf('%s',$max_files);
+    }
+
+    public function getOpcacheValidateTimestamps()
+    {
+        return opcache_get_configuration()['directives']['opcache.validate_timestamps'];
+    }
 }
